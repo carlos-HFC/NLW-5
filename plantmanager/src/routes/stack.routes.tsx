89aa4e48e139b7bfcf6eb78aa2830e4d6a2e1993
Carlos Faustino <chfcchfc96@gmail.com>
2021-04-21 20:12:18 -0300

    Nova rota

--- plantmanager/src/routes/stack.routes.tsx
+++ plantmanager/src/routes/stack.routes.tsx
@@ -2,17 +2,19 @@ import React, { FC } from 'react'
 import { createStackNavigator } from "@react-navigation/stack";
 
 import colors from '../styles/colors'
-import { Welcome } from '../pages/Welcome';
-import { UserIdentification } from '../pages/UserIdentification';
 import { Confirmation } from '../pages/Confirmation';
+import { PlantSelect } from '../pages/PlantSelect';
+import { UserIdentification } from '../pages/UserIdentification';
+import { Welcome } from '../pages/Welcome';
 
 const { Navigator, Screen } = createStackNavigator()
 
 const AppRoutes: FC = () => (
   <Navigator headerMode="none" screenOptions={{ cardStyle: { backgroundColor: colors.white } }}>
-    <Screen name="Welcome" component={Welcome} />
-    <Screen name="UserIdentification" component={UserIdentification} />
     <Screen name="Confirmation" component={Confirmation} />
+    <Screen name="PlantSelect" component={PlantSelect} />
+    <Screen name="UserIdentification" component={UserIdentification} />
+    <Screen name="Welcome" component={Welcome} />
   </Navigator>
 )
 
