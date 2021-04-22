b48158018639f2b9f54a5998f836826a37408d85
Carlos Faustino <chfcchfc96@gmail.com>
2021-04-22 19:37:12 -0300

    Tipagem do episódio

---
+++ podcastr/src/@types/index.d.ts
@@ -0,0 +1,11 @@
+type Episode = {
+  id: string
+  duration: number
+  title: string
+  thumbnail: string
+  members: string
+  publishedAt: string
+  durationAsString: string
+  description: string
+  url: string
+}
