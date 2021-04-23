df45f619f628b98fb895a991ed9d2e6af75b9d51
Carlos Faustino <chfcchfc96@gmail.com>
2021-04-23 18:51:07 -0300

    .

--- podcastr/src/components/Header/index.tsx
+++ podcastr/src/components/Header/index.tsx
@@ -1,13 +1,13 @@
 import { format } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
 
-import style from './_header.module.scss'
+import styles from './_header.module.scss'
 
 export const Header = () => {
   const currenteDate = format(new Date(), 'EEEEEE, d MMM', { locale: ptBR })
 
   return (
-    <header className={style.container}>
+    <header className={styles.container}>
       <img src="/logo.svg" alt="Podcastr" />
 
       <p>O melhor para você ouvir, sempre</p>
