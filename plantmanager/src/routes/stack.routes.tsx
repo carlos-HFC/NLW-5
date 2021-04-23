b54bcde6db7bb9dc4fae34e5d78a23a25aaeb61f
Carlos Faustino <chfcchfc96@gmail.com>
2021-04-23 12:35:00 -0300

    Verificar se há o nome do usuário salvo na storage para que entre no app sem precisar preencher o nome novamente

--- plantmanager/src/routes/stack.routes.tsx
+++ plantmanager/src/routes/stack.routes.tsx
@@ -1,4 +1,5 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createStackNavigator } from "@react-navigation/stack";
 
 import AuthRoutes from './tab.routes'
@@ -10,15 +11,35 @@ import colors from '../styles/colors'
 
 const { Navigator, Screen } = createStackNavigator()
 
-const AppRoutes: FC = () => (
-  <Navigator headerMode="none" screenOptions={{ cardStyle: { backgroundColor: colors.white } }}>
-    <Screen name="Welcome" component={Welcome} />
-    <Screen name="UserIdentification" component={UserIdentification} />
-    <Screen name="Confirmation" component={Confirmation} />
-    <Screen name="PlantSelect" component={AuthRoutes} />
-    <Screen name="PlantSave" component={PlantSave} />
-    <Screen name="MyPlants" component={AuthRoutes} />
-  </Navigator>
-)
+const AppRoutes: FC = () => {
+  const [user, setUser] = useState('')
 
+  useEffect(() => {
+    AsyncStorage.getItem("@plantmanager:user")
+      .then(data => setUser(data || ''))
+  }, [])
+
+  return (
+    <Navigator headerMode="none" screenOptions={{ cardStyle: { backgroundColor: colors.white } }}>
+      {user === '' || !user
+        ? (
+          <>
+            <Screen name="Welcome" component={Welcome} />
+            <Screen name="UserIdentification" component={UserIdentification} />
+            <Screen name="Confirmation" component={Confirmation} />
+            <Screen name="PlantSelect" component={AuthRoutes} />
+            <Screen name="PlantSave" component={PlantSave} />
+            <Screen name="MyPlants" component={AuthRoutes} />
+          </>
+        ) : (
+          <>
+            <Screen name="PlantSelect" component={AuthRoutes} />
+            <Screen name="PlantSave" component={PlantSave} />
+            <Screen name="MyPlants" component={AuthRoutes} />
+          </>
+        )}
+    </Navigator>
+  )
+
+}
 export default AppRoutes
